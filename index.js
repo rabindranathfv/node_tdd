8fe398f42590b59b5373b7daad8cb2e868514f61
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-03-03 17:12:06 -0300

    functional crud for test

--- index.js
+++ index.js
@@ -11,12 +11,38 @@ app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
     const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
-    res.json(data);
+    res.json({
+      ok: true,
+      data
+    });
 });
 
-app.post('/', async() => {
-  const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
-  res.send(data);
+app.post('/', async(req, res) => {
+  const { body } = req;
+  const {data} = await axios.post('https://jsonplaceholder.typicode.com/users', body);
+  res.json({
+    ok: true,
+    data
+  });
+});
+
+app.put('/:id', async(req, res) => {
+  const { id } = req.params;
+  const { body } = req;
+  await axios.put(`https://jsonplaceholder.typicode.com/users/${id}`, body);
+  res.json({
+    ok: true,
+    id
+  });
+});
+
+app.delete('/:id', async(req, res) => {
+  const { id } = req.params;
+  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+  res.json({
+    ok: true,
+    id
+  });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
