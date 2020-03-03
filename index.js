19e1fd3ecfa0042aacfb525951d26166d5b37bfd
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-03-03 16:29:28 -0300

    add post method for users

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ const express = require('express');
 const axios = require('axios');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
 
 app.get('/', async (req, res) => {
-    const {data} = axios.get('https://jsonplaceholder.typicode.com/users');
-    res.send('Hello World!')
+    const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+    res.json(data);
+});
+
+app.post('/', async() => {
+  const {data} = await axios.get('https://jsonplaceholder.typicode.com/users');
+  res.send(data);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
