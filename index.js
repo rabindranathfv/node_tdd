736c4231cf1b43fa9d14f6e8ee7ed6b8a638ea76
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-03-03 16:06:41 -0300

    using axios for external data

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', async (req, res) => {
+    const {data} = axios.get('https://jsonplaceholder.typicode.com/users');
+    res.send('Hello World!')
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
