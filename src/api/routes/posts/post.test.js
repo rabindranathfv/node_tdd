65f8012dd4522b85e07b0a7737d846407d44fb7c
Rabindranath Ferreira <ferreiralox@gmail.com>
2020-03-18 20:55:10 -0300

    add assertions for endpoints of posts

--- src/api/routes/posts/post.test.js
+++ src/api/routes/posts/post.test.js
@@ -9,8 +9,11 @@ describe('BACKEND Test', () => {
                     .send({ userId: 5})
                     .set('user_id', 1)
                     .set('Content-type', 'application/json');
-            console.log('REQUEST BY SUPERTEST:::',req);
-            
+            console.log('REQUEST POST BY SUPERTEST:::',req.body);
+            console.log('REQUEST POST BY SUPERTEST:::',req.request.method);
+            console.log('REQUEST POST BY SUPERTEST:::',req.status);
+            expect(req.status).toBe(200);
+            expect(req.body).toEqual({ ok: true, post: { userId: 5, id: 101 } });
         });
     })
 });
